37f18441bcb2b303db5fb2913f3bc6a5a834927c
Lucrecia Parodi <parodilucrecia@gmail.com>
2025-05-19 18:38:27 +0000

    navbar

--- src/js/hero.jsx
+++ src/js/hero.jsx
@@ -3,11 +3,11 @@ import { Links } from "./redirect_links";
 
 export default function Hero() {
   return (
-    
+
     <section id="hero" className="container d-flex flex-column justify-content-center align-items-center text-center pt-5">
       <h1 className="display-4 mt-5 b-2">Hi! I'm</h1>
       <h1 className="nametext text-primary fw-bold mt-4">Lucrecia Parodi</h1>
-      <h2 className="h4 mt-4">Full Stack Developer</h2>
+      <h2 className="h4 mt-4"><em>Full Stack Developer</em></h2>
       <p className="mt-4 px-3 px-md-5">
         I love building clean, responsive, and accessible web interfaces. <br />
         I'm looking for opportunities to grow, learn, and collaborate on real projects. <br />
@@ -21,7 +21,7 @@ export default function Hero() {
           Contact Me
         </a>
       </div>
-      <Links/>
+      <Links />
     </section>
   );
 }
